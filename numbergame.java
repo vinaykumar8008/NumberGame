512764016e14c1054ecd7ac72f59fd9351b30c2a
vinaykumar <vinaynetha8008@gmail.com>
2025-03-08 18:42:29 +0530

    Add files via upload

---
+++ numbergame.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class numbergame {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        boolean playAgain = true;
+
+        while (playAgain) {
+            int randomNumber = random.nextInt(100) + 1; // Generates number between 1 and 100
+            int attempts = 0, maxAttempts = 10;
+            boolean guessedCorrectly = false;
+
+            System.out.println("Guess the number between 1 and 100. You have " + maxAttempts + " attempts.");
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int guess = scanner.nextInt();
+                attempts++;
+
+                if (guess == randomNumber) {
+                    System.out.println("Congratulations! You guessed the correct number in " + attempts + " attempts.");
+                    guessedCorrectly = true;
+                    break;
+                } else if (guess < randomNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (!guessedCorrectly) {
+                System.out.println("You've used all attempts! The correct number was: " + randomNumber);
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            String response = scanner.next();
+            playAgain = response.equalsIgnoreCase("yes");
+        }
+
+        System.out.println("Thank you for playing!");
+        scanner.close();
+    }
+}
